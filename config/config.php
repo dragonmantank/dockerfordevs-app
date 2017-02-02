20ba8c2015e47edb8f467ebf9d9244ccd642a30f
Chris Tankersley <chris@ctankersley.com>
2017-02-02 09:30:01 -0500

    Updated Zend Expressive

--- config/config.php
+++ config/config.php
@@ -4,13 +4,10 @@ use Zend\Stdlib\ArrayUtils;
 use Zend\Stdlib\Glob;
 
 /**
- * Configuration files are loaded in a specific order. First ``global.php`` and afterwards ``local.php``. This way
- * local settings overwrite global settings.
+ * Configuration files are loaded in a specific order. First ``global.php``, then ``*.global.php``.
+ * then ``local.php`` and finally ``*.local.php``. This way local settings overwrite global settings.
  *
  * The configuration can be cached. This can be done by setting ``config_cache_enabled`` to ``true``.
- *
- * The configuration is stored in json so it is not depended on 3rd party libraries. Feel free to use something else
- * like Zend\Config\Writer to write PHP arrays.
  *
  * Obviously, if you use closures in your config you can't cache it.
  */
@@ -20,7 +17,7 @@ $cachedConfigFile = 'data/cache/app_config.php';
 $config = [];
 if (is_file($cachedConfigFile)) {
     // Try to load the cached config
-    $config = json_decode(file_get_contents($cachedConfigFile), true);
+    $config = include $cachedConfigFile;
 } else {
     // Load configuration from autoload path
     foreach (Glob::glob('config/autoload/{{,*.}global,{,*.}local}.php', Glob::GLOB_BRACE) as $file) {
@@ -29,10 +26,10 @@ if (is_file($cachedConfigFile)) {
 
     // Cache config if enabled
     if (isset($config['config_cache_enabled']) && $config['config_cache_enabled'] === true) {
-        file_put_contents($cachedConfigFile, json_encode($config));
+        file_put_contents($cachedConfigFile, '<?php return ' . var_export($config, true) . ';');
     }
 }
 
 // Return an ArrayObject so we can inject the config as a service in Aura.Di
 // and still use array checks like ``is_array``.
-return new \ArrayObject($config, \ArrayObject::ARRAY_AS_PROPS);
+return new ArrayObject($config, ArrayObject::ARRAY_AS_PROPS);
--- config/container.php
+++ config/container.php
@@ -4,10 +4,11 @@ use Zend\ServiceManager\Config;
 use Zend\ServiceManager\ServiceManager;
 
 // Load configuration
-$config = require 'config.php';
+$config = require __DIR__ . '/config.php';
 
 // Build container
-$container = new ServiceManager(new Config($config['dependencies']));
+$container = new ServiceManager();
+(new Config($config['dependencies']))->configureServiceManager($container);
 
 // Inject config
 $container->setService('config', $config);
